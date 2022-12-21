ac434b8ca48c44d268d44a57b518cbfb817b9608
kilocharlie96 <cernak.kiko@gmail.com>
2022-12-21 20:28:26 +0100

    vytvaram novy filter, video 59:37

--- src/main/java/sk/kikocernak/filtre/FirstFilter.java
+++ src/main/java/sk/kikocernak/filtre/FirstFilter.java
@@ -1,8 +1,12 @@
 package sk.kikocernak.filtre;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 import java.io.IOException;
-
+@WebFilter(urlPatterns = "/*",
+        filterName = "FirstFilter name",
+        initParams = {@WebInitParam(name = "meno", value = "Kikusko Cernak")})
 public class FirstFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -11,7 +15,7 @@ public class FirstFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        
+
     }
 
     @Override
